35a45f01b8e220a3ac451774dd7bc79085d73d83
Xavi <xavigarcia9870@gmail.com>
2022-04-11 00:42:36 -0500

    Estilos escritorio prueba juego

---
+++ public/escritorios/actividad/juegos/js/navBarJuego.js
@@ -0,0 +1,49 @@
+const dibujarNavBar = (accesos = [], titulo) => {
+    let navBarHtml = '';
+
+    const role = (rol === 'PRO_ROLE')
+        ? 'pro'
+        : 'est';
+
+    navBarHtml += `
+        <nav id=barraNav>
+            <input type="checkbox" id="check">
+
+            <div id="titulo">
+                <h1>${titulo}</h1>
+            </div>
+
+            <label for="check" class="checkbtn" id="barras">
+                    <i class="fas fa-bars"></i>
+            </label>
+    `;
+    navBarHtml += `
+            <div class="enlace">
+                <img src="${baseUrl}/images/logoPNG.png" alt="EDUMEX" class="logo">
+            </div>
+    `;
+    navBarHtml += `
+            <ul id="lista">
+    `;
+    accesos.forEach(({ nombre, referencia },index) => {
+        navBarHtml += `
+            <li><a id="${role}${index}" href="${referencia}" class="botonesNav">${nombre}</a></li>
+        `;
+    });
+    navBarHtml += `
+                <li><a id="salirJuego" href class="botonesNav">Salir</a></li>
+            </ul>
+        </nav>
+    `;
+
+    navBar.innerHTML = navBarHtml;
+
+    /*const lista = document.querySelector("#lista");
+
+    for(i = 0; i < (lista.children.length - 1); i++){
+        document.querySelector(`#${role}${i}`).addEventListener('click', (event) => {
+            event.preventDefault();
+            showPop();
+        });
+    }*/
+}
